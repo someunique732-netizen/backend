e8e919fc98597b27cd6d8da34cb7fc9c80f30c11
someunique732-netizen <someunique732@gmail.com>
2026-06-02 09:56:24 +0545

    Inital Commit

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = "backend.deployment_setting" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "backend.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
