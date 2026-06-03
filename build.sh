02ef4895c3f7f74fc381285540e7f1c8b1a2331f
someunique732-netizen <someunique732@gmail.com>
2026-06-03 10:23:39 +0545

    superuser

--- build.sh
+++ build.sh
@@ -2,10 +2,27 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
 
+python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --noinput 
-fi
+# Create superuser safely
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
+else:
+    print("Superuser env vars not set, skipping")
+EOF
