6314f2c88af2360cd085b2cc3e82069336e7107e
someunique732-netizen <someunique732@gmail.com>
2026-06-03 10:09:14 +0545

    superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]; then
+    python manage.py createsuperuser --noinput 
+fi
