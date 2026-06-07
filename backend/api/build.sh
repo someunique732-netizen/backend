eb779ef557694309e88eced679ca60eefaf42666
someunique732-netizen <someunique732@gmail.com>
2026-06-07 10:26:40 +0545

    last

--- backend/api/build.sh
+++ backend/api/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
-set -o errexit  # stop script if any command fails
+set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations --noinput || true
+python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput
-python manage.py migrate
